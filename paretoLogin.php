559c68a42979c7730fb63921430efd59a2dfe185
Acopum <gjdeleon@hotmail.ca>
2018-02-12 21:03:27 -0500

    Added actual functionality to login page

--- paretoLogin.php
+++ paretoLogin.php
@@ -12,18 +12,17 @@
 </h1>
 
 <?php
-$username = $password = "";
-
-function clean_login($data)
-{
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
+    //clean submitted data for dangerous characters
+    function clean_login($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
 ?>
 
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+<form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
     <div class="container">
         <label><b>Username:</b></label>
         <input type="text" placeholder="" name="username" required>
@@ -36,22 +35,63 @@ function clean_login($data)
 </form>
 
 <?php
+    $username = $password = "";
 
-if($_SERVER["REQUEST_METHOD"] == "POST")
-{
-    $username = clean_login($_POST["username"]);
-    $password = clean_login($_POST["password"]);
+    if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $username = clean_login($_POST["username"]);
+        $password = clean_login($_POST["password"]);
 
-    if ($username == "admin" && $password =="pass")
-    {
-        header("Location: ParetoMainMenu.php");
-        exit();
+        /*
+        //log in for actual server
+        $server = "192.168.194.154";
+        $port = "3306";
+        $username = "";
+        $password = "";
+        $dbname = "";
+
+
+        //create connection to DB
+        $connection = new mysqli($server, $username, $password, $dbname, $port);
+
+        //test connection
+        if($connection->connect_error)
+        {
+            die("Connection failure. Error code: ".$connection->connect_error);
+        }
+
+        //store queries in variable
+        $sql_check = "SELECT pass FROM login WHERE username = $username";
+
+        //store results in variable
+        $sqlpass = $connection->query($sql_check);
+
+        //retrieve first row of query
+        $retrieveDB=$sqlpass->fetch_assoc()
+
+        //check credentials
+        if ($password == $retrieveDB["pass"])
+        {
+            header("Location: ParetoMainMenu.php");
+            exit();
+        }
+        else
+        {
+            echo "Login credentials invalid. Please try again.";
+        }
+        */
+
+        //check credentials
+        if ($username == "admin" && $password =="pass")
+        {
+            header("Location: ParetoMainMenu.php");
+            exit();
+        }
+        else
+        {
+            echo "Login credentials invalid. Please try again.";
+        }
     }
-    else
-    {
-        echo "Login credentials invalid. Please try again.";
-    }
-}
 ?>
 
 </body>
